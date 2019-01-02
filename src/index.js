52a1d0e81aedbf94274bbab1ec0540595d862a9a
Aykut Uludağ <aykutuludag1@gmail.com>
2019-01-02 05:12:29 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FuelSpot_ADMIN from './App';
+import * as serviceWorker from './serviceWorker';
+import '../node_modules/underscore/underscore-min.js';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+
+// Add these imports - Step 1
+import { Provider } from 'react-redux';
+import { store } from './redux';
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <FuelSpot_ADMIN />
+    </Provider>,
+    document.getElementById('FUELSPOT_VIEW')
+);
+
+
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
---
+++ src/services/PostData.js
@@ -0,0 +1,29 @@
+export function PostData(username, password) {
+
+    const url = "https://fuel-spot.com/api/admin/login.php";
+    const body = "username=" + username + "&password=" + password + "&AUTH_KEY=FPh76g0MSZ2okeWQmShYDlXakjgjhbej";
+    const params ={
+        headers: {
+            "content-type" : "application/x-www-form-urlencoded"
+        },
+        body: body,
+        method: "POST"
+    };
+    console.log(username,password);
+
+    return new Promise((resolve, reject) =>{
+
+        fetch(url, params)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    //Kullanıcı bulundu
+                    resolve(result);
+                },
+                (error) => {
+                    reject();
+                }
+            );
+
+    });
+}
